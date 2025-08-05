b97eca542e55058dd8312a7e5b020b84b1e23c04
johnialexdch-dot <johnialexdch@gmail.com>
2025-08-05 11:56:44 +0300

    main.py
    Signed-off-by: johnialexdch-dot <johnialexdch@gmail.com>

--- zamunda_api/main.py
+++ zamunda_api/main.py
@@ -116,4 +116,5 @@ def stream(type: str, id: str, request: Request):
     return streams
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0")  # Махни port=8000
+
